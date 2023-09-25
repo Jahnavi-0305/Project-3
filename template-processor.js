5ec01dc5d09c863bd6a9c7fc0da87f57b88dba78
Satya Suhas <89505583+suhasss077@users.noreply.github.com>
2023-09-25 12:25:41 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,26 @@
-TableTemplate.fillIn('table', { month: 'January', day: '30', year: '2016' }, 'Part Number');
+'use strict';
+
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(dictionary) {
+        let returnString = this.template;
+        for (const property in dictionary) {
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                const regex = new RegExp("{{" + property + "}}", "g");
+                returnString = returnString.replace(regex, dictionary[property]);
+            }
+        }
+        const regex = /{{.*?}}/g; // Updated regex to match non-greedy
+        returnString = returnString.replace(regex, "");
+        return returnString;
+    }
+}
+
+// Example usage:
+const template = "Hello, {{name}}! Today is {{day}}.";
+const dictionary = { name: "Alice", day: "Monday" };
+const processor = new TemplateProcessor(template);
+const result = processor.fillIn(dictionary);
+console.log(result);
