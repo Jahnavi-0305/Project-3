4320ba7bce704b58384d85930f0786b42e8a149d
Satya Suhas <89505583+suhasss077@users.noreply.github.com>
2023-09-25 12:25:28 -0400

    Update TableTemplate.js
    Updated Table Template

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,26 +1,58 @@
-'use strict';
+"use strict";
+(function () {
+    class TableTemplate {
+        static fillIn(tId, dict, NameCol) {
+            const table = document.getElementById(tId);
 
-class TemplateProcessor {
-    constructor(template) {
-        this.template = template;
-    }
-    fillIn(dictionary) {
-        let returnString = this.template;
-        for (const property in dictionary) {
-            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
-                const regex = new RegExp("{{" + property + "}}", "g");
-                returnString = returnString.replace(regex, dictionary[property]);
+            const headerRow = table.rows[0];
+            const isPartNumberTable = NameCol === 'Part Number' && tId === 'table1';
+
+            // eslint-disable-next-line array-callback-return
+            Array.from(headerRow.cells).map(cell => {
+                const columnHeader = cell.textContent.trim();
+                // eslint-disable-next-line max-len
+                cell.textContent = this.fillCellTemplate(columnHeader, dict, columnHeader, isPartNumberTable);
+            });
+            // eslint-disable-next-line max-len
+            const columnIndex = NameCol ? Array.from(headerRow.cells).findIndex(cell => cell.textContent.trim() === NameCol) : -1;
+
+            for (let i = 1; i < table.rows.length; i++) {
+                const row = table.rows[i];
+
+                if (columnIndex !== -1) {
+                    const cell = row.cells[columnIndex];
+                    // eslint-disable-next-line max-len
+                    cell.textContent = this.fillCellTemplate(cell.textContent, dict, NameCol, false);
+                } else {
+                    Array.from(row.cells).forEach((cell, j) => {
+                        const currentNameCol = headerRow.cells[j].textContent.trim();
+                        // eslint-disable-next-line max-len
+                        cell.textContent = this.fillCellTemplate(cell.textContent, dict, currentNameCol, isPartNumberTable);
+                    });
+
+                }
             }
-        }
-        const regex = /{{.*?}}/g; // Updated regex to match non-greedy
-        returnString = returnString.replace(regex, "");
-        return returnString;
-    }
-}
 
-// Example usage:
-const template = "Hello, {{name}}! Today is {{day}}.";
-const dictionary = { name: "Alice", day: "Monday" };
-const processor = new TemplateProcessor(template);
-const result = processor.fillIn(dictionary);
-console.log(result);
+            table.style.visibility = "visible";
+        }
+
+        static fillCellTemplate(cellContent, dict, NameCol, isPartNumberTable) {
+            const regex = /{{(.*?)}}/g;
+
+            return cellContent.replace(regex, (match, property) => {
+                // eslint-disable-next-line no-nested-ternary
+                const replacement = isPartNumberTable && NameCol === 'Part Number' && property.startsWith('n')
+                    ? `{{${property}}}`
+                    // eslint-disable-next-line no-prototype-builtins
+                    : dict.hasOwnProperty(property)
+                        ? dict[property]
+                        : `{{${property}}}`;
+
+                return replacement;
+
+            });
+        }
+    }
+
+    window.TableTemplate = TableTemplate;
+}());
